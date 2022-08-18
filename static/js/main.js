fede70ff6473caa993ad509cd5935feedc25ee28
Andres Meza <andresmezad2519@gmail.com>
2022-08-17 23:58:01 -0500

    Cards and Modal

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,14 @@
-console.log("Work");
+console.log('Work');
+const openModal = document.querySelector('.open-modal');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+openModal.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.classList.remove('modal--show');
+});
